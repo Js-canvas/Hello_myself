ed2cc0b1807f5584b7f4b4e4e9382be9b857b7f5
Js-canvas <501868392@qq.com>
2016-12-13 21:24:26 +0800

    aa

--- C3与H5/模块化/hello.js
+++
@@ -1,6 +0,0 @@
-define(function(require,exports,module){
-    var sayHello = function(){
-        console.log("this is hello.js");
-    }
-    exports.sayHello = sayHello;
-});
--- C3与H5/模块化/hi.js
+++
@@ -1,7 +0,0 @@
-define(function(require,exports,modile){
-    var hello = require("./hello");
-    exports.fn = function(){
-        console.log("this is hi.js");
-        hello.sayHello();
-    }
-})
